3e1d33432f6f4a1b7a7a4f34474bfd5d3cb4607e
mediastore93 <nik@MacBook-Pro-7.local>
2017-09-26 19:04:56 -0700

    tab

--- pir.py
+++ pir.py
@@ -26,28 +26,24 @@ try:
 
     # Loop until PIR output is 0
     while GPIO.input(GPIO_PIR)==1:
-    Current_State  = 0
-
+        Current_State  = 0
     print ("  Ready")
-
     # Loop until users quits with CTRL-C
     while True :
-
     # Read PIR state
-    Current_State = GPIO.input(GPIO_PIR)
-
+        Current_State = GPIO.input(GPIO_PIR)
     if Current_State==1 and Previous_State==0:
     # PIR is triggered
-    start_time=time.time()
-    time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    print (" Motion detected @ %s !" % time_now)
-    # Record previous state
-    Previous_State=1
+        start_time=time.time()
+        time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print (" Motion detected @ %s !" % time_now)
+        # Record previous state
+        Previous_State=1
     elif Current_State==0 and Previous_State==1:
-    # PIR has returned to ready state
-    stop_time=time.time()
-    print ("  Ready "),
-    Previous_State=0
+        # PIR has returned to ready state
+        stop_time=time.time()
+        print ("  Ready "),
+        Previous_State=0
 
 except KeyboardInterrupt:
     print ("  Quit")
