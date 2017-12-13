8b3ba73d5fc2c6bdd801a92e5dae2e92336fffc6
mediastore93 <mediastore93@gmail.com>
2017-12-13 08:11:48 -0800

    Update pir_pi.py

--- pir_pi.py
+++ pir_pi.py
@@ -1,7 +1,9 @@
 from gpiozero import MotionSensor
+from datetime import datetime
 
 pir = MotionSensor(4)
 
 while True
   pir.wait_for_motion()
-  print("Motion Detected")
+  time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+  print (" Motion detected @ %s !" % time_now)
