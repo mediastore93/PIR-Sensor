151741fdf8bb3b10acda5d5808b7d7e05935fbac
mediastore93 <mediastore93@gmail.com>
2017-12-13 08:35:19 -0800

    Update pir_santa.py

--- pir_santa.py
+++ pir_santa.py
@@ -5,5 +5,10 @@ pir = MotionSensor(4)
 
 while True:
   pir.wait_for_motion()
-  time_now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+  time_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
   print (" Motion detected @ %s !" % time_now)
+  pir.wait_for_no_motion()
+  time_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+  print (" Motion stopped @ %s !" % time_now)
+  
+  
